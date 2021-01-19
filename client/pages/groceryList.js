732517eeaac53c251d2b828e62c84df32cd3da4f
Faraz Akhtar <faraz@Farazs-Air.fios-router.home>
2021-01-19 18:42:18 -0500

    added baseline for routes

---
+++ client/pages/groceryList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GroceryList = () => <div>Grocery List Works!</div>;
+
+// class GroceryList extends React.PureComponent {
+//   render() {
+//     return <div>Grocery List Works!</div>;
+//   }
+// }
+
+export default GroceryList;
