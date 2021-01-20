db7ae509744430c248d38ace61745f1c17917ed1
Heidi Kim <heidiyoorakim@gmail.com>
2021-01-20 18:40:42 -0500

    updated Add Item Modal and Fridge/Grocery container to create the correct payload for action creator

--- client/components/AddItem.jsx
+++ client/components/AddItem.jsx
@@ -1,4 +1,81 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const AddItem = ({ itemName, location, addItemHandler, userID, householdID }) => {
+
+  const [itemName, setItemName] = useState("");   // item name
+  const [priority, setPriority] = useState(null);    // priority
+  const [shared, setShared] = useState(false);    // shareable
+  const [fridge, setFridge] = useState(null);
+  const [grocery, setGrocery] = useState(null);
+  /* we need to pass this object as an argument to our addItemHandler:
+  {
+    itemName: string,
+    priority: number,
+    shared: boolean,
+    grocery: boolean,
+    fridge: boolean,
+    userID: number,
+    householdID: number/null,
+  }
+*/
+
+if (location === 'fridge') {
+    setFridge(true);
+    setGrocery(false);
+  } else if (location === 'grocery') {
+    setGrocery(true);
+    setFridge(false);
+  }
+
+
+  return (
+    <div className='addItemContainer'>
+      <h2>Add an Item</h2>
+      <div>
+        <input type="text" 
+              placeholder="item" 
+              value={itemName} 
+              onChange={(e) => setItemName(e.target.value)} required>
+        </input>
+
+        <select id="add-item-select" className='dropdown' defaultValue={null} onChange={(e) => setPriority(e.target.value)}>
+          <option value={null}>priority (optional)</option>
+          <option value={1}>high</option>
+          <option value={2}>medium</option>
+          <option value={3}>low</option>
+        </select>
+
+        <label for="shared"> Shareable? </label>
+        <input type="checkbox" id="shared" value={true} onChange={(e) => setShared(e.target.value)}></input>
+
+      <button id='FinalAddItemBtn'onClick={() =>{
+        const payloadObj = {
+          itemName: itemName,
+          priority: number,
+          shared: shared,
+          grocery: grocery,
+          fridge: fridge,
+          userID: userID,
+          householdID: householdID,
+        };
+
+        addItemHandler(payloadObj)}
+        }>Add</button>
+      </div>
+    </div>
+  )
+};
+
+export default AddItem;
+
+
+
+
+
+
+/*
+original AddItem
+
 
 const AddItem = ({setFetched}) => {
   const [itemName, setItemName] = useState("");   // item name
@@ -58,4 +135,13 @@ const AddItem = ({setFetched}) => {
   )
 };
 
-export default AddItem;
+
+      <select id="add-item-select" className='dropdown' defaultValue={assignedList} onChange={(event) => setAssignedList(event.target.value)} required>
+        <option value="" disabled>add to: </option>
+        <option value="fridge">fridge</option>
+        <option value="grocery">grocery</option>
+      </select>
+
+
+
+*/
