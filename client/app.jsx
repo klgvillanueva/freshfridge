7d7953257886ca3ff57264d133e203b0d32f8967
Faraz Akhtar <faraz@Farazs-Air.fios-router.home>
2021-01-20 12:34:17 -0500

    set-up boilerplate

--- client/app.jsx
+++ client/app.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavBar from './components/NavBar.jsx';
 import RoutesForApp from './routes/routes.js';
 
-// import Household from './pages/householdPage.jsx';
-// import Login from './pages/loginPage.jsx';
-// import User from './pages/userPage.jsx';
-// import SignUp from './pages/signupPage.jsx';
+const mapStateToProps = (state) => {
+  //navbar state
+};
 
-//import Routes from './routes/routes';
-
-// const mapStateToProps = (state) => {
-//   //navbar state
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   // dispatchers to update navbar state depending on what is clicked in navbar
-// };
+const mapDispatchToProps = (dispatch) => {
+  // dispatchers to update navbar state depending on what is clicked in navbar
+};
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -24,59 +17,18 @@ class App extends React.Component {
   }
 
   render() {
-
-    return (
-      <div>
-        <div>Hello</div>
-      </div>
-    );
-  }
-}
-
-export default App;
-//export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-/*
-
-    /*
-    let displayNavBar;
+    /**
+     *     
+     * let displayNavBar;
     if (this.props.isLoggedIn){
       displayNavBar = <NavBar />
     } else {
       displayNavBar = null;
     }
-*/
-<div className='appHeader'>
-        <Link to="/user"> 
-            <img
-              id="logo"
-              src="./assets/fflogo.png"
-              alt="Fresh Fridge Logo"
-              height='150px'
-              width='250px'
-            ></img>
-        </Link>
-        <NavBar />
-        <hr />
-        <Routes />
-      </div>
+     */
 
-
-
-
-
-
-
-
-        
-
-import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
-import Household from './pages/householdPage.jsx';
-import Login from './pages/loginPage.jsx';
-import User from './pages/userPage.jsx';
-import SignUp from './pages/signupPage.jsx';      
-
-      <div className="appHeader">
+    return (
+      <div>
         <Link to="/user">
           <img
             id="logo"
@@ -87,16 +39,12 @@ import SignUp from './pages/signupPage.jsx';
           ></img>
         </Link>
         <NavBar />
-        <Router>
-          <Switch>
-            <Route exact path="/user" component={User} />
-            <Route exact path="/household" component={Household} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/" component={Login} />
-          </Switch>
-        </Router>
+        <hr />
+        <RoutesForApp />
       </div>
+    );
+  }
+}
 
-
-
-*/
+export default App;
+//export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/pages/404Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class NoMatch404Error extends React.PureComponent {
-  render() {
-    return <div>This is the noMatch.js File</div>;
-  }
-}
-
-export default NoMatch404Error;
