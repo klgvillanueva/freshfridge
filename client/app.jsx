361215dcac8db16ae4a6d65d6d99b4ffa46a9f44
Faraz Akhtar <faraz@Farazs-Air.fios-router.home>
2021-01-20 12:24:05 -0500

    test

--- client/app.jsx
+++ client/app.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-<<<<<<< HEAD
-import NavBar from './components/NavBar';
-import Routes from './routes/routes';
-=======
 import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar.jsx';
-//import Routes from './routes/routes.js'
->>>>>>> 2220278229a50f87a42b6c2afdc60784a5f6dac4
+import RoutesForApp from './routes/routes.js';
 
-import Household from './pages/householdPage.jsx';
-import Login from './pages/loginPage.jsx';
-import User from './pages/userPage.jsx';
-import SignUp from './pages/signupPage.jsx';
+// import Household from './pages/householdPage.jsx';
+// import Login from './pages/loginPage.jsx';
+// import User from './pages/userPage.jsx';
+// import SignUp from './pages/signupPage.jsx';
 
 //import Routes from './routes/routes';
 
-const mapStateToProps = (state) => {
-  //navbar state 
-};
+// const mapStateToProps = (state) => {
+//   //navbar state
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  // dispatchers to update navbar state depending on what is clicked in navbar
-}
+// const mapDispatchToProps = (dispatch) => {
+//   // dispatchers to update navbar state depending on what is clicked in navbar
+// };
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -29,42 +24,11 @@ class App extends React.Component {
   }
 
   render() {
-/*
-    let displayNavBar;
-    if (this.props.isLoggedIn){
-      displayNavBar = <NavBar />
-    } else {
-      displayNavBar = null;
-    }
-*/
+
     return (
-
-        <div className='appHeader'>
-          <Link to="/user"> 
-              <img
-                id="logo"
-                src="./assets/fflogo.png"
-                alt="Fresh Fridge Logo"
-                height='150px'
-                width='250px'
-              ></img>
-          </Link>
-          <NavBar />
-          <Router>
-      <Switch>
-          <Route exact path="/user" component={User} />
-          <Route exact path="/household" component={Household} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/" component={Login} />
-        </Switch>
-</Router>
-          
-        
-            
-          
-        </div>
-
-
+      <div>
+        <div>Hello</div>
+      </div>
     );
   }
 }
@@ -72,20 +36,16 @@ class App extends React.Component {
 export default App;
 //export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-
-
-
 /*
-original:
 
-// import Household from './pages/householdPage.jsx';
-
-// import Login from './pages/loginPage.jsx';
-// import User from './pages/userPage.jsx';
-
-// import User from './pages/userPage.jsx';
-// import SignUp from './pages/signupPage.jsx';
-
+    /*
+    let displayNavBar;
+    if (this.props.isLoggedIn){
+      displayNavBar = <NavBar />
+    } else {
+      displayNavBar = null;
+    }
+*/
 <div className='appHeader'>
         <Link to="/user"> 
             <img
@@ -108,21 +68,34 @@ original:
 
 
 
+        
+
 import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Household from './pages/householdPage.jsx';
 import Login from './pages/loginPage.jsx';
 import User from './pages/userPage.jsx';
 import SignUp from './pages/signupPage.jsx';      
 
-
-<Router>
-      <Switch>
-          <Route exact path="/user" component={User} />
-          <Route exact path="/household" component={Household} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/" component={Login} />
-        </Switch>
-</Router>
+      <div className="appHeader">
+        <Link to="/user">
+          <img
+            id="logo"
+            src="./assets/fflogo.png"
+            alt="Fresh Fridge Logo"
+            height="150px"
+            width="250px"
+          ></img>
+        </Link>
+        <NavBar />
+        <Router>
+          <Switch>
+            <Route exact path="/user" component={User} />
+            <Route exact path="/household" component={Household} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/" component={Login} />
+          </Switch>
+        </Router>
+      </div>
 
 
 
--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { Route, Link, Switch as Router } from 'react-router-dom';
-import GroceryList from '../pages/groceryList';
-import Login from '../pages/login';
-import ShoppingList from '../pages/shoppingList';
-import SignUp from '../pages/signup';
-import MainContainer from '../Container/MainContainer.jsx';
-import NoMatch404Error from '../pages/404Error';
-=======
 import { Route, Switch as Router } from 'react-router-dom';
 
 // import pages for routes
@@ -18,40 +9,23 @@ import User from '../pages/userPage.jsx';
 import SignUp from '../pages/signupPage.jsx';
 import NoMatch404Error from '../pages/404Error.jsx';
 
->>>>>>> 2220278229a50f87a42b6c2afdc60784a5f6dac4
-
 const RoutesForApp = () => (
   <Router>
     <Route exact path="/">
       <Login />
     </Route>
-<<<<<<< HEAD
-    <Route path="/GroceryList">
-      <GroceryList />
+    <Route path="/user">
+      <User />
     </Route>
-    <Route path="/ShoppingList">
-      <ShoppingList />
+    <Route path="/household">
+      <Household />
     </Route>
-    <Route path="/SignUp">
-      <MainContainer />
-    </Route>
-    <Route>
-      <NoMatch404Error />
-    </Route>
-=======
-    <Route path="/user" >
-      <User/> 
-    </Route>
-    <Route path="/household"> 
-      <Household/> 
-    </Route>
-    <Route path="/signup"> 
-      <SignUp/> 
+    <Route path="/signup">
+      <SignUp />
     </Route>
     <Route>
       <NoMatch404Error />
-    </Route>  
->>>>>>> 2220278229a50f87a42b6c2afdc60784a5f6dac4
+    </Route>
   </Router>
 );
 
